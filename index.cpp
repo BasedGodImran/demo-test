3c56bcc1a3619ab2f77d30a31f8fec67d92304c5
BasedGodImran <basedgodimran@gmail.com>
2021-10-04 20:56:14 -0700

    init

--- index.cpp
+++ index.cpp
@@ -6,13 +6,13 @@ int main() {
     const int N = 10;
     double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
 
-    // address of first element in array is simple numbers
+    
     double *ptr = numbers;
 
     for (int i = 0; i < N; ++i) {
-        // print value pointed to by ptr
+       
         cout << *ptr << " ";
-        // move to next element in array
+       
         ptr++;
     }
     cout << endl;
